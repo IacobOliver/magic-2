78e06b45e2039dee3247ce8d78c9813724261c48
Oliver <olialexander08@gmail.com>
2024-04-18 14:01:23 +0300

    almost finished intex

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
+const manageTraineriVideos = () =>{
+    let videoTrainers = document.querySelectorAll(".video-trainer")
+    videoTrainers.forEach(function(videoTrainer) {
+        let iframe = videoTrainer.querySelector("iframe").contentWindow.document
+        let videoElement = iframe.querySelector("video")
+        videoElement.autoplay = false
+        videoElement.controls = false
+    });
+ 
 
-// let videos = document.querySelectorAll(".video-trainer")
-
-// for (const video of videos) {
-//     video.addEventListener("click",(e) => controlVideo(e, "playVideo"))
-// }
-
-// function controlVideo(e, vidFunction) {
-//     // Find the iframe element within the clicked video-trainer div
-//     let iframe = e.currentTarget.querySelector("iframe");
-//     console.log(iframe)
-
-//     // Check if the iframe element exists
-//     if (iframe) {
-//         // Access the iframe's contentWindow to control the video
-//         let iframeWindow = iframe.contentWindow;
-//         iframeWindow.postMessage(
-//             '{"event":"command","func":"' + vidFunction + '","args":""}',
-//             "*"
-//         );
-//     }
-// }
-document.addEventListener("DOMContentLoaded", function() {
+    //pause and play video from carrousel
     let buttons = document.querySelectorAll(".startVideo");
-
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function () {
             let videoIndex = parseInt(this.getAttribute("data-video"));
             // Select the corresponding iframe based on the index
             let iframe = document.querySelectorAll(".video-trainer iframe")[videoIndex];
 
-            
-                let iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
 
-                let videoElement = iframeDocument.querySelector("video");
-                console.log(videoElement.paused)
-                // Check if the video element exists
-                if (videoElement.paused) {
-                    // Play the video
-                    videoElement.play();
-                } else {
-                    videoElement.pause();
-                }
-           
+            let iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+            let videoElement = iframeDocument.querySelector("video");
+            console.log(videoElement.paused)
+            // Check if the video element exists
+            if (videoElement.paused) {
+                // Play the video
+                videoElement.play();
+            } else {
+                videoElement.pause();
+            }
+
         });
     });
+}
+
+
+const slider = document.querySelector('.slide-track');
+slider.addEventListener('animationiteration', () => {
+  slider.style.animation = 'none';  // Temporarily disable the animation
+  requestAnimationFrame(() => {
+    slider.style.transform = 'translateX(0)';  // Reset the transform
+    requestAnimationFrame(() => {
+      slider.style.animation = '';  // Re-enable the animation
+    });
+  });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    manageTraineriVideos();
+
+});
+
+
+
+
 
