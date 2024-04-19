1e2e6f3fb614f15967b476637d51fde92d484020
Oliver <olialexander08@gmail.com>
2024-04-19 10:57:22 +0300

    universul main windows page done

--- main.js
+++ main.js
@@ -9,22 +9,22 @@ function scrollToId(id) {
   );
 }
 
-var swiper = new Swiper(".alegi-swiper", {
-  lazy: true,
-  preloadImages: false,
-  loop: true,
-  slidesPerView: "auto",
-  centeredSlides: true,
-  spaceBetween: 10,
-  effect: 'coverflow',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2000,
-  },
-});
+// var swiper = new Swiper(".alegi-swiper", {
+//   lazy: true,
+//   preloadImages: false,
+//   loop: true,
+//   slidesPerView: "auto",
+//   centeredSlides: true,
+//   spaceBetween: 10,
+//   effect: 'coverflow',
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 2000,
+//   },
+// });
 
 var swiper = new Swiper(".traineri-swiper", {
   lazy: true,
